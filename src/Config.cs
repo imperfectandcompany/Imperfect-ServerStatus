d04ec24fe7041ad2adf490b4302cea0c2024aeb3
raz <9876747+tomatoedev@users.noreply.github.com>
2024-02-09 16:02:51 -0600

    Create config and webhook models

---
+++ src/Config.cs
@@ -0,0 +1,18 @@
+﻿using CounterStrikeSharp.API.Core;
+using IGDiscord.Models;
+
+namespace IGDiscord
+{
+    public class Config : BasePluginConfig
+    {
+        /// <summary>
+        /// List of Webhook URL's and prefixes for Discord messages
+        /// </summary>
+        public List<Webhook>? Webhooks { get; set; }
+
+        /// <summary>
+        /// Interval in seconds that this Webhook is called
+        /// </summary>
+        public int LogInterval { get; set; }
+    }
+}
---
+++ src/Models/Webhook.cs
@@ -0,0 +1,9 @@
+﻿namespace IGDiscord.Models
+{
+    public class Webhook
+    {
+        public string? MessagePrefix { get; set; }
+
+        public string? WebHookUrl { get; set; }
+    }
+}
