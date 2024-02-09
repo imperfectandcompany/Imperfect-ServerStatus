92f35b892379a56f24f1878afd17a6cb465eb307
raz <9876747+tomatoedev@users.noreply.github.com>
2024-02-09 16:34:48 -0600

    Create method to get or create config on load

--- src/IGDiscordPlugin.cs
+++ src/IGDiscordPlugin.cs
@@ -1,4 +1,6 @@
 ﻿using CounterStrikeSharp.API.Core;
+using IGDiscord.Models;
+using System.Text.Json;
 
 namespace IGDiscord;
 
@@ -8,4 +10,56 @@ public class IGDiscordPlugin : BasePlugin
     public override string ModuleVersion => "1.0.0";
     public override string ModuleAuthor => "raz";
     public override string ModuleDescription => "Plugin for Discord Webhooks for Imperfect Gamers";
+
+    private static Config? _config;
+
+    public override void Unload(bool hotReload)
+    {
+        base.Unload(hotReload);
+    }
+
+    public override void Load(bool hotReload)
+    {
+        GetOrCreateConfig();
+    }
+
+    private void GetOrCreateConfig()
+    {
+        var configPath = Path.Join(ModuleDirectory, "Config.json");
+
+        if (!File.Exists(configPath))
+        {
+            // This is the first time running this plugin, create the config file
+            var configData = new Config()
+            {
+                Webhooks = new List<Webhook>()
+                {
+                    new Webhook()
+                    {
+                        MessagePrefix = "### SAMPLE MESSAGE PREFIX ###",
+                        WebHookUrl = "https://discord.com/api/webhooks/###############/#################",
+                    }
+                },
+                LogInterval = 300
+            };
+
+            var jsonOptions = new JsonSerializerOptions
+            {
+                WriteIndented = true,
+                Encoder = System.Text.Encodings.Web.JavaScriptEncoder.UnsafeRelaxedJsonEscaping
+            };
+
+            var serializedConfigData = JsonSerializer.Serialize(configData, jsonOptions);
+
+            File.WriteAllText(configPath, serializedConfigData);
+
+            _config = configData;
+        }
+        else
+        {
+            var configData = File.ReadAllText(configPath);
+
+            _config = JsonSerializer.Deserialize<Config>(configData);
+        }
+    }
 }
