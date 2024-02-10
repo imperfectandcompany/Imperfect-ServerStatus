d6b1fa3e45c42f8b8cc43a1b54846317b9c58b62
raz <9876747+tomatoedev@users.noreply.github.com>
2024-02-10 12:21:35 -0600

    Add message id prop

--- src/Models/MessageInfo/ServerStatusMessageInfo.cs
+++ src/Models/MessageInfo/ServerStatusMessageInfo.cs
@@ -3,6 +3,11 @@
     public class ServerStatusMessageInfo : MessageInfo
     {
         /// <summary>
+        /// Message ID of server status message if already posted
+        /// </summary>
+        public string? MessageId { get; set; }
+
+        /// <summary>
         /// Interval in seconds that this Webhook is called. 0 if not repeated.
         /// </summary>
         public int MessageInterval { get; set; } = 0;
