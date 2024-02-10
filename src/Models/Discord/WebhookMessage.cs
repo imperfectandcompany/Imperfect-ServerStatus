b59830bbfe0835d328a944dc3daa4180bf80c457
raz <9876747+tomatoedev@users.noreply.github.com>
2024-02-10 12:10:33 -0600

    Create webhook msg model to match Discord conventions

---
+++ src/Models/Discord/WebhookMessage.cs
@@ -0,0 +1,17 @@
+﻿using IGDiscord.Models.Discord;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IGDiscord.src.Models.Discord
+{
+    public class WebhookMessage
+    {
+        public string? Content { get; set; }
+
+        public required List<Embed> Embeds { get; set; }
+
+    }
+}
