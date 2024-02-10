2254fdccd6c0fb7ebbcd1ac6466153b88c13db3a
raz <9876747+tomatoedev@users.noreply.github.com>
2024-02-10 17:23:36 -0600

    Create config service, move config related mthds

--- src/IGDiscordPlugin.cs
+++ src/IGDiscordPlugin.cs
@@ -14,12 +14,14 @@ public class IGDiscordPlugin : BasePlugin
     public override string ModuleAuthor => "raz";
     public override string ModuleDescription => "Plugin for Discord Webhooks for Imperfect Gamers";
 
-    private static Config? _config;
-    private static readonly DiscordService _discordService;
+    public Config? _config;
+    private readonly DiscordService _discordService;
+    private readonly ConfigService _configService;
 
-    static IGDiscordPlugin()
+    public IGDiscordPlugin()
     {
         _discordService = new DiscordService();
+        _configService = new ConfigService();
     }
 
     public override void Unload(bool hotReload)
@@ -29,7 +31,7 @@ public class IGDiscordPlugin : BasePlugin
 
     public override void Load(bool hotReload)
     {
-        GetOrCreateConfig();
+        _config = _configService.LoadConfig(ModuleDirectory);
 
         if (_config != null)
         {
@@ -47,46 +49,5 @@ public class IGDiscordPlugin : BasePlugin
         {
             Util.PrintError("The config file did not load correctly. Please check that there is a config.json file in the plugin directory.");
         };
-    }
-
-    private void GetOrCreateConfig()
-    {
-        var configPath = Path.Join(ModuleDirectory, "Config.json");
-
-        if (!File.Exists(configPath))
-        {
-            // This is the first time running this plugin, create the config file
-            var configData = new Config()
-            {
-                ServerStatusMessage = new ServerStatusMessageInfo()
-                {
-                    Type = Constants.MessageType.ServerStatus,
-                    Prefix = "### SAMPLE SERVER STATUS MESSAGE PREFIX ###",
-                    WebhookUri = "https://discord.com/api/webhooks/###############/#################",
-                    MessageInterval = 300
-                }
-            };
-
-            var jsonOptions = new JsonSerializerOptions
-            {
-                WriteIndented = true,
-                Encoder = System.Text.Encodings.Web.JavaScriptEncoder.UnsafeRelaxedJsonEscaping
-            };
-
-            var serializedConfigData = JsonSerializer.Serialize(configData, jsonOptions);
-            File.WriteAllText(configPath, serializedConfigData);
-
-            Util.PrintLog("Created config.json file");
-
-            _config = configData;
-        }
-        else
-        {
-            var configData = File.ReadAllText(configPath);
-
-            Util.PrintLog("Read config.json file");
-
-            _config = JsonSerializer.Deserialize<Config>(configData);
-        }
     }
 }
---
+++ src/Services/ConfigService.cs
@@ -0,0 +1,98 @@
+﻿using IGDiscord.Models.MessageInfo;
+using IGDiscord.Models;
+using IGDiscord.Utils;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace IGDiscord.Services
+{
+    public class ConfigService
+    {
+        private Config? _config { get; set; }
+
+        public Config? LoadConfig(string moduleDirectory)
+        {
+            var configPath = Path.Join(moduleDirectory, "Config.json");
+
+            if (configPath != null)
+            {
+                if (!File.Exists(configPath))
+                {
+                    // This is the first time running this plugin, create the config file
+                    _config = CreateConfig(configPath);
+                }
+                else
+                {
+                    // Config exists, get it
+                    _config = GetConfig(configPath);
+                }
+
+                if (_config == null)
+                {
+                    Util.PrintError($"Something went wrong when retrieving config.json.");
+                }
+            }
+
+            return _config;
+        }
+
+        private Config? CreateConfig(string configPath)
+        {
+            try
+            {
+                var configData = new Config()
+                {
+                    ServerStatusMessage = new ServerStatusMessageInfo()
+                    {
+                        MessageType = Constants.MessageType.ServerStatus,
+                        Prefix = "### SAMPLE SERVER STATUS MESSAGE PREFIX ###",
+                        WebhookUri = "https://discord.com/api/webhooks/###############/#################",
+                        MessageInterval = 300
+                    }
+                };
+
+                var jsonOptions = new JsonSerializerOptions
+                {
+                    WriteIndented = true,
+                    Encoder = System.Text.Encodings.Web.JavaScriptEncoder.UnsafeRelaxedJsonEscaping
+                };
+
+                var serializedConfigData = JsonSerializer.Serialize(configData, jsonOptions);
+                File.WriteAllText(configPath, serializedConfigData);
+
+                _config = configData;
+                Util.PrintLog(configData.ServerStatusMessage.WebhookUri);
+
+                Util.PrintLog("Created config.json file");
+            }
+            catch (Exception ex)
+            {
+                Util.PrintError($"Error creating config.json: {ex}");
+            }
+
+            return _config;
+        }
+
+        private Config? GetConfig(string configPath)
+        {
+            try
+            {
+                var configData = File.ReadAllText(configPath);
+
+                Util.PrintLog("Read config.json file");
+
+                _config = JsonSerializer.Deserialize<Config>(configData);
+            }
+            catch (Exception ex)
+            {
+                Util.PrintError($"Error reading config.json: {ex}");
+            }
+
+            return _config;
+        }
+    }
+}
