2470e985e96e4c0aa1f8106962aaa12a5c97b005
raz <9876747+tomatoedev@users.noreply.github.com>
2024-02-10 12:11:41 -0600

    Create helper mthds for snake casing properties when serializing json

---
+++ src/Helpers/JsonHelper.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace IGDiscord.Helpers
+{
+    public class SnakeCaseNamingPolicy : JsonNamingPolicy
+    {
+        public static SnakeCaseNamingPolicy Instance { get; } = new SnakeCaseNamingPolicy();
+
+        public override string ConvertName(string name)
+        {
+            return name.ToSnakeCase();
+        }
+    }
+}
---
+++ src/Helpers/TextHelpers.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IGDiscord.Helpers
+{
+    public static class TextHelpers
+    {
+        public static string ToSnakeCase(this string str)
+        {
+            return string.Concat(str.Select((x, i) => i > 0 && char.IsUpper(x) ? "_" + x.ToString() : x.ToString())).ToLower();
+        }
+    }
+}
