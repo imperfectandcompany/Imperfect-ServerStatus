8f70428fc6050718cabff71509124160a0f270e6
raz <9876747+tomatoedev@users.noreply.github.com>
2024-02-10 14:55:08 -0600

    Move discord embed models to their own dir, create webhook resp model

---
+++ src/Models/Discord/WebhookResponse.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IGDiscord.src.Models.Discord
+{
+    internal class WebhookResponse
+    {
+        public string Id { get; set; }
+    }
+}
