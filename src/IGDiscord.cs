10010f3904c33e75b820de631497c602bdb19533
raz <9876747+tomatoedev@users.noreply.github.com>
2024-02-11 12:38:22 -0600

    Add cs# plugin config interface, implement parse mthd

--- src/IGDiscord.cs
+++ src/IGDiscord.cs
@@ -1,19 +1,21 @@
-﻿using CounterStrikeSharp.API.Core;
-using IGDiscord.Models.MessageInfo;
+﻿using IGDiscord.Models.MessageInfo;
 using IGDiscord.Models;
-using IGDiscord.Services;
 using IGDiscord.Utils;
-using System.Text.Json;
+using CounterStrikeSharp.API.Core;
 using IGDiscord.Services.Interfaces;
+using Microsoft.Extensions.Logging;
+using CounterStrikeSharp.API.Modules.Config;
 
 namespace IGDiscord;
 
-public class IGDiscord : BasePlugin
+public partial class IGDiscord : BasePlugin, IPluginConfig<Config>
 {
     public override string ModuleName => "IGDiscord";
     public override string ModuleVersion => "1.0.0";
     public override string ModuleAuthor => "raz";
     public override string ModuleDescription => "A Discord webhook plugin for Imperfect Gamers";
+
+    public Config Config { get; set; }
 
     public Config? _config;
     private readonly IConfigService _configService;
@@ -47,5 +49,23 @@ public class IGDiscord : BasePlugin
         {
             Util.PrintError("The config file did not load correctly. Please check that there is a config.json file in the plugin directory.");
         };
+    }
+
+    public void OnConfigParsed(Config config)
+    {
+        if (config.ServerStatusMessage == null)
+        {
+            config.ServerStatusMessage = new StatusMessageInfo()
+            {
+                MessageType = Constants.MessageType.ServerStatus,
+                Prefix = "### SAMPLE SERVER STATUS MESSAGE PREFIX ###",
+                WebhookUri = "https://discord.com/api/webhooks/###############/#################",
+                MessageInterval = 300
+            };
+        }
+
+        ConfigManager.Load<Config>(ModuleName);
+
+        Config = config;
     }
 }
