c66641eabe4bd3dcddef32538544cf5779e6e3d8
raz <9876747+tomatoedev@users.noreply.github.com>
2024-02-11 12:01:02 -0600

    Add server status msg to interface

--- src/Services/Interfaces/IDiscordService.cs
+++ src/Services/Interfaces/IDiscordService.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using IGDiscord.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,8 @@ using System.Threading.Tasks;
 
 namespace IGDiscord.Services.Interfaces
 {
-    internal interface IDiscordService
+    public interface IDiscordService
     {
+        Task SendServerStatusMessage(Config config);
     }
 }
