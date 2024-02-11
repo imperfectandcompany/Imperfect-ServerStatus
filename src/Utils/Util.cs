b38b5d9bf32a23a33cf7d4d55e117615af0beba3
raz <9876747+tomatoedev@users.noreply.github.com>
2024-02-11 11:54:42 -0600

    Update plugin name on log msgs

--- src/Utils/Util.cs
+++ src/Utils/Util.cs
@@ -7,12 +7,12 @@ namespace IGDiscord.Utils
     {
         public static void PrintLog(string message)
         {
-            Console.WriteLine($"\u001b[34m[IGDiscordPlugin] \u001b[32m{message}\u001b[0m");
+            Console.WriteLine($"\u001b[34m[IGDiscord] \u001b[32m{message}\u001b[0m");
         }
 
         public static void PrintError(string message)
         {
-            Console.WriteLine($"\u001b[34m[IGDiscordPlugin:ERROR] \u001b[32m{message}\u001b[0m");
+            Console.WriteLine($"\u001b[34m[IGDiscord:ERROR] \u001b[32m{message}\u001b[0m");
         }
     }
 }
