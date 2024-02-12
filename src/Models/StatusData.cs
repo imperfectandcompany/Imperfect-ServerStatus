5d54aebfbffcd3294b03cfd979280961d93e9121
raz <9876747+tomatoedev@users.noreply.github.com>
2024-02-12 12:26:28 -0600

    Create status data model

---
+++ src/Models/StatusData.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IGDiscord.Models
+{
+    public class StatusData
+    {
+        public string MapName { get; set; } = "";
+
+        public DateTime Timestamp { get; set; }
+    }
+}
