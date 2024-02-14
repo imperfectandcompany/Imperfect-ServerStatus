d0e9210c2666083cb354c52ce91deeee2616e15d
raz <9876747+tomatoedev@users.noreply.github.com>
2024-02-14 11:28:05 -0600

    Add server name, ip address, and online status props

--- src/Models/StatusData.cs
+++ src/Models/StatusData.cs
@@ -8,6 +8,12 @@ namespace IGDiscord.Models
 {
     public class StatusData
     {
+        public string ServerName { get; set; } = "";
+
+        public string IpAddress { get; set; } = "";
+        
+        public bool ServerOnline { get; set; } = true;
+
         public string MapName { get; set; } = "";
 
         public DateTime Timestamp { get; set; }
